b465dc66a8ddda3e9195f5fb593220523f54cc35
liviucozac <liviucozac28@gmail.com>
2026-05-29 19:34:32 +0300

    fix: add RNFirebaseAsStaticFramework and allow non-modular includes for Firebase v21

--- withFirebaseModularHeaders.js
+++ withFirebaseModularHeaders.js
@@ -9,19 +9,31 @@ module.exports = function withFirebaseModularHeaders(config) {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let podfile = fs.readFileSync(podfilePath, 'utf8');
 
+      // Required for @react-native-firebase v21 + useFrameworks: static
+      if (!podfile.includes('$RNFirebaseAsStaticFramework')) {
+        podfile = podfile.replace(
+          /use_frameworks!/,
+          `$RNFirebaseAsStaticFramework = true\nuse_frameworks!`
+        );
+      }
+
       const patch = `
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
       config.build_settings['DEFINES_MODULE'] = 'YES'
+      config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
       config.build_settings['OTHER_CFLAGS'] = '$(inherited) -Wno-non-modular-include-in-framework-module'
       config.build_settings['OTHER_CPLUSPLUSFLAGS'] = '$(inherited) -Wno-non-modular-include-in-framework-module'
     end
   end`;
 
-      podfile = podfile.replace(
-        /post_install do \|installer\|/,
-        `post_install do |installer|\n${patch}`
-      );
+      if (!podfile.includes('DEFINES_MODULE')) {
+        podfile = podfile.replace(
+          /post_install do \|installer\|/,
+          `post_install do |installer|\n${patch}`
+        );
+      }
+
       fs.writeFileSync(podfilePath, podfile);
 
       return config;
